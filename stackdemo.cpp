8344156132009d88f87f9492fbdd7101b7afbe1e
TsalemisIasonas <tsalemisi@gmail.com>
2023-03-30 19:00:48 +0300

    update

--- stackdemo.cpp
+++ stackdemo.cpp
@@ -49,8 +49,8 @@ public:
     }
 
     T pop() {
-        T lastElement = s[stackSize];
         stackSize-=1;
+        T lastElement = s[stackSize];
         return lastElement;
     }
     int size()
@@ -91,20 +91,20 @@ int main()
      t = s;
     cout << "popping from s: " << s.pop() << endl;
     //  s.push(8);
-    //  stack<int> a(s);
-    //  t.push(99);
-    //  a.push(77);
-    //  cout << "s: " << s << endl;
-    //  cout << "t: " << t << endl;
-    //  cout << "a: " << a << endl;
-    //  // now with doubles...
-    //  stack<double> c(4);
-    //  c.push(3.14);
-    //  c.push(1.414);
-    //  cout << "c contains doubles " << c << endl;
-    //  // and with characters...
-    //  stack<char> k(4);
-    //  k.push('$');
-    //  cout << "k contains a character " << k << endl;
+     stack<int> a(s);
+     t.push(99);
+     a.push(77);
+     cout << "s: " << s << endl;
+     cout << "t: " << t << endl;
+     cout << "a: " << a << endl;
+     // now with doubles...
+     stack<double> c(4);
+     c.push(3.14);
+     c.push(1.414);
+     cout << "c contains doubles " << c << endl;
+     // and with characters...
+     stack<char> k(4);
+     k.push('$');
+     cout << "k contains a character " << k << endl;
 }
 #endif
