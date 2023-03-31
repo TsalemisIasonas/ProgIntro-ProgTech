ed4198f98060472aba85ab190b8a2ce45c46029c
TsalemisIasonas <tsalemisi@gmail.com>
2023-03-31 12:33:43 +0300

    update

--- stackdemo.cpp
+++ stackdemo.cpp
@@ -1,100 +1,88 @@
-#include <iostream>
-
-using namespace std;
-
-template <typename T>
-class stack
-{
-public:
-    stack(int size)
-    {
-        this->stackSize = 0;
-        this->requestedSize = size;
-        this->s = new T[requestedSize];
-    }
-    stack(const stack &s)
-    {
-        this->stackSize = s.stackSize;
-        this->requestedSize = s.requestedSize;
-        this->s = new T[requestedSize];
-        for (int i = 0; i < requestedSize; i++)
-        {
-            this->s[i] = s.s[i];
-        }
-    }
-    ~stack()
-    {
-        delete[] s;
-    }
-
-    const stack &operator=(const stack &s)
-    {
-        if (this != &s) // check for self-assignment
-        {
-            delete[] this->s;
-            this->stackSize = 0; // reset stackSize
-            this->requestedSize = s.requestedSize;
-            this->s = new T[requestedSize];
-            for (int i = 0; i < s.stackSize; i++)
-            {
-                this->push(s.s[i]);
-            }
-        }
-        return *this;
-    }
-    {
-
-        delete[] this->s;
-        this->stackSize = s.stackSize;
-        this->requestedSize = s.requestedSize;
-        this->s = new T[requestedSize];
-        for (int i = 0; i < stackSize; i++)
-        {
-            this->push(s.s[i]);
-        }
-        return *this;
-    }
-
-    bool empty()
-    {
-        if (stackSize != 0)
-            return false;
-        return true;
-    }
-    void push(const T &x)
-    {
-        s[stackSize] = x;
-        stackSize++;
-    }
-
-    T pop()
-    {
-        stackSize -= 1;
-        T lastElement = s[stackSize];
-        return lastElement;
-    }
-    int size()
-    {
-        return stackSize;
-    }
-
-    friend ostream &operator<<(ostream &out, const stack &s)
-    {
-        out << '[';
-        if (s.stackSize > 0)
-        {
-            for (int i = 0; i < s.stackSize - 1; i++)
-            {
-                out << s.s[i] << ", ";
-            }
-            out << s.s[s.stackSize - 1];
-        }
-        out << ']';
-        return out;
-    }
-
-private:
-    int stackSize;
-    int requestedSize;
-    T *s;
+#include <iostream>  
+  
+using namespace std;  
+  
+template <typename T>  
+class stack  
+{  
+public:  
+    stack(int size)  
+    {  
+        this->stackSize = 0;  
+        this->requestedSize = size;  
+        this->s = new T[requestedSize];  
+    }  
+    stack(const stack &s)  
+    {  
+        this->stackSize = s.stackSize;  
+        this->requestedSize = s.requestedSize;  
+        this->s = new T[requestedSize];  
+        for (int i = 0; i < requestedSize; i++)  
+        {  
+            this->s[i] = s.s[i];  
+        }  
+    }  
+    ~stack()  
+    {  
+        delete[] s;  
+    }  
+  
+    const stack &operator=(const stack &s)  
+    {  
+  
+        delete[] this->s;  
+        this->stackSize = s.stackSize;  
+        this->requestedSize = s.requestedSize;  
+        this->s = new T[requestedSize];  
+        for (int i = 0; i < stackSize; i++)  
+        {  
+            this->s[i] = s.s[i];  
+        }  
+        return *this;  
+    }  
+  
+    bool empty()  
+    {  
+        if (stackSize != 0)  
+            return false;  
+        return true;  
+    }  
+    void push(const T &x)  
+    {  
+        if (stackSize < requestedSize)  
+        {  
+            s[stackSize] = x;  
+            stackSize++;  
+        }  
+    }  
+  
+    T pop()  
+    {  
+        stackSize -= 1;  
+        return s[stackSize];  
+    }  
+    int size()  
+    {  
+        return stackSize;  
+    }  
+  
+    friend ostream &operator<<(ostream &out, const stack &s)  
+    {  
+        out << '[';  
+        if (s.stackSize > 0)  
+        {  
+            for (int i = 0; i < s.stackSize - 1; i++)  
+            {  
+                out << s.s[i] << ", ";  
+            }  
+            out << s.s[s.stackSize - 1];  
+        }  
+        out << ']';  
+        return out;  
+    }  
+  
+private:  
+    int stackSize;  
+    int requestedSize;  
+    T* s;  
 };
