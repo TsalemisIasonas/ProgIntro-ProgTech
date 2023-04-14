5134e7567fe64c85ecbe7e02b76acaf32c025775
TsalemisIasonas <tsalemisi@gmail.com>
2023-04-14 22:47:32 +0300

    upadate

--- specker_move_state.cpp
+++ specker_move_state.cpp
@@ -15,7 +15,7 @@ public:
     friend ostream &operator<<(ostream &out, const Move &move);
 
 private:
-    int sh, sc,th,tc;
+    int sh, sc, th, tc;
 };
 
 class State
@@ -30,7 +30,7 @@ public:
     void next(const Move &move) throw(logic_error);
     bool winning() const;
 
-    int getHeaps() const;   
+    int getHeaps() const;
     int getCoins(int h) const throw(logic_error);
 
     int getPlayers() const;
@@ -40,28 +40,52 @@ public:
 
 private:
     int h;
-    const int* c;
+    const int *c;
     int n;
 };
 
-Move::Move(int sh, int sc, int th, int tc): sh(sh),sc(sc),th(th),tc(tc){}
-int Move::getSource() const{return sh;}
-int Move :: getSourceCoins() const {return sc;}
-int Move :: getTarget() const {return th;}
-int Move :: getTargetCoins() const {return tc;}
+Move::Move(int sh, int sc, int th, int tc) : sh(sh), sc(sc), th(th), tc(tc) {}
+int Move::getSource() const { return sh; }
+int Move ::getSourceCoins() const { return sc; }
+int Move ::getTarget() const { return th; }
+int Move ::getTargetCoins() const { return tc; }
 
-ostream &operator<<(ostream &out, const Move &move){
-        if (move.getTargetCoins() !=0){
+ostream &operator<<(ostream &out, const Move &move)
+{
+    if (move.getTargetCoins() != 0)
+    {
         out << "takes " << move.sc << " coins from heap " << move.sh << " and puts " << move.tc << " coins to heap " << move.th;
-        }
-        else  out << "takes " << move.sc << " coins from heap " << move.sh << " and puts nothing";
-        return out;
     }
+    else
+    {
+        out << "takes " << move.sc << " coins from heap " << move.sh << " and puts nothing";
+    }
+    return out;
+}
+
+State::State(int h, const int c[], int n) : h(h), c(c), n(n) {}
+State::~State() { delete c; }
+bool State::winning() const {
+    for (int i=0; i<h; i++){
+        if (c[i] != 0) return false;
+    }
+    return true;
+}
+int State::getHeaps() const { return h; }
+int State::getCoins(int h) const throw(logic_error)
+{
+    if (h < 0 || h > this->h)
+        throw logic_error("invalid heap");
+    else
+        return c[h];
+}
+int State::getPlayers() const { return n; }
 
 
-
-
-int main(){
-    Move move1 = Move(10,3,5,0);
-    cout << move1;
+int main()
+{
+    int array[6] = {1, 2, 3, 4, 5, 6};
+    State state = State(4, array, 5);
+    int val = state.getCoins(3);
+    cout << val;
 }
