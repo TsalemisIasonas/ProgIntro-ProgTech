582273f5616ad3057be8992d9fc536bc0518f1ae
TsalemisIasonas <tsalemisi@gmail.com>
2023-04-14 12:03:00 +0300

    update

--- specker_move_state.cpp
+++ specker_move_state.cpp
@@ -5,33 +5,55 @@ class Move
 {
 public:
     // Take sc coins from heap sh and put tc coins to heap th.
-    Move(int sh, int sc, int th, int tc);
+    Move(int sh, int sc, int th, int tc): sh(sh),sc(sc),th(th),tc(tc){}
 
-    int getSource() const;
-    int getSourceCoins() const;
-    int getTarget() const;
-    int getTargetCoins() const;
+    int getSource() const{
+        return sh;
+    }
+    int getSourceCoins() const{
+        return sc;
+    }
+    int getTarget() const{
+        return th;
+    }
+    int getTargetCoins() const{
+        return tc;
+    }
 
-    friend ostream &operator<<(ostream &out, const Move &move);
+    friend ostream &operator<<(ostream &out, const Move &move){
+        if (move.getTargetCoins() !=0){
+        out << "takes " << move.sc << " coins from heap " << move.sh << " and puts " << move.tc << " coins to heap " << move.th;
+        }
+        else  out << "takes " << move.sc << " coins from heap " << move.sh << " and puts nothing";
+        return out;
+    }
+
+private:
+    int sh, sc,th,tc;
 };
 
-class State
-{
-public:
-    // State with h heaps, where the i-th heap starts with c[i] coins.
-    // A total of n players are in the game, numbered from 0 to n-1,
-    // and player 0 is the first to play.
-    State(int h, const int c[], int n);
-    ~State();
+// class State
+// {
+// public:
+//     // State with h heaps, where the i-th heap starts with c[i] coins.
+//     // A total of n players are in the game, numbered from 0 to n-1,
+//     // and player 0 is the first to play.
+//     State(int h, const int c[], int n);
+//     ~State();
 
-    void next(const Move &move) throw(logic_error);
-    bool winning() const;
+//     void next(const Move &move) throw(logic_error);
+//     bool winning() const;
 
-    int getHeaps() const;   
-    int getCoins(int h) const throw(logic_error);
+//     int getHeaps() const;   
+//     int getCoins(int h) const throw(logic_error);
 
-    int getPlayers() const;
-    int getPlaying() const;
+//     int getPlayers() const;
+//     int getPlaying() const;
 
-    friend ostream &operator<<(ostream &out, const State &state);
-};
+//     friend ostream &operator<<(ostream &out, const State &state);
+// };
+
+int main(){
+    Move move1 = Move(10,3,5,0);
+    cout << move1;
+}
