73f37324ca4e4eb3b4b589e57841329394923887
TsalemisIasonas <tsalemisi@gmail.com>
2023-05-05 00:38:04 +0300

    chessboard

--- chessboard.cpp
+++ chessboard.cpp
@@ -102,13 +102,14 @@ public:
     }
 };
 
-// int main() {
-//  ChessBoardArray a(4, 1); // size 4x4, rows and columns numbered from 1
-//  a[3][1] = 42;
-//  a[4][4] = 17;
-//  try { a[2][1] = 7; }
-//  catch(out_of_range &e) { cout << "a[2][1] is black" << endl; }
-//  try { cout << a[1][2] << endl; }
-//  catch(out_of_range &e) { cout << "and so is a[1][2]" << endl; }
-//  cout << a;
-//  }
+
+
+
+
+
+
+
+
+
+
+
