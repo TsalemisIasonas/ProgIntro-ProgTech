7782d3a5f3209f0ce58046974065d9e4d9b74887
TsalemisIasonas <tsalemisi@gmail.com>
2023-05-06 18:49:08 +0300

    addTerm

--- polynomial.cpp
+++ polynomial.cpp
@@ -10,7 +10,7 @@ protected:
         int exponent;
         int coefficient;
         Term *next;
-        Term(int exp, int coeff, Term *n):exponent(exp),coefficient(coeff),next(n){}
+        Term(int exp, int coeff, Term *n) : exponent(exp), coefficient(coeff), next(n) {}
         friend class Polynomial;
         friend Polynomial operator+(const Polynomial &p, const Polynomial &q);
         friend Polynomial operator*(const Polynomial &p, const Polynomial &q);
@@ -19,56 +19,111 @@ protected:
     };
     Term *head;
 
-    void insertEnd(int coeff, int exp){
-    if (head==nullptr){
-        head = new Term(exp, coeff, nullptr);
-        return;
+    void insertEnd(int coeff, int exp)
+    {
+        if (head == nullptr)
+        {
+            head = new Term(exp, coeff, nullptr);
+            return;
+        }
+        Term *temp = head;
+        while (temp->next != nullptr)
+            temp = temp->next;
+        temp->next = new Term(exp, coeff, nullptr);
     }
-    Term *temp = head;
-    while(temp->next!=nullptr)
-        temp=temp->next;
-    temp->next = new Term(exp, coeff, nullptr);
-}
-
 
 public:
-    Polynomial(){
+    Polynomial()
+    {
         head = nullptr;
     }
-    Polynomial(const Polynomial &p){
+    Polynomial(const Polynomial &p)
+    {
         head = nullptr;
-        for (Term *i=p.head; i!=nullptr; i = i->next){
+        for (Term *i = p.head; i != nullptr; i = i->next)
+        {
             insertEnd(i->coefficient, i->exponent);
         }
     }
-    ~Polynomial();
+    ~Polynomial()
+    {
+        Term *current = head;
+        Term *next;
+        while (current != nullptr)
+        {
+            next = current->next;
+            delete current;
+            current = next;
+        }
+    }
 
     Polynomial &operator=(const Polynomial &p);
 
-        void addTerm(int expon, int coeff);
-        double evaluate(double x);
+    void addTerm(int expon, int coeff)
+    {
+        Term *newTerm = new Term(expon, coeff, nullptr);
 
-        friend Polynomial operator+(const Polynomial &p, const Polynomial &q);
-        friend Polynomial operator*(const Polynomial &p, const Polynomial &q);
+        if (head == nullptr)
+        {
+            head = newTerm;
+            return;
+        }
 
-        friend ostream &operator<<(ostream &out, const Polynomial &p);
-    };
+        if (expon > head->exponent)
+        {
+            newTerm->next = head;
+            head = newTerm;
+            return;
+        }
+
+        Term *current = head;
+        while (current->next != nullptr && expon < current->next->exponent)
+        {
+            current = current->next;
+        }
+
+        newTerm->next = current->next;
+        current->next = newTerm;
+    }
+
+    double evaluate(double x){
+        
+    }
+
+    friend Polynomial operator+(const Polynomial &p, const Polynomial &q);
+    friend Polynomial operator*(const Polynomial &p, const Polynomial &q);
+
+    friend ostream &operator<<(ostream &out, const Polynomial &p)
+    {
+        for (Term *i = p.head; i != nullptr; i = i->next)
+        {
+            if (i->exponent == 1)
+            {
+                out << i->coefficient << 'x';
+            }
+            else
+                out << i->coefficient << 'x^' << i->exponent;
+        }
+        out << endl;
+        return out;
+    }
+};
 
 int main()
-    {
-        Polynomial p;    // 0
-        p.addTerm(1, 3); // 3x
+{
+    Polynomial p;    // 0
+    p.addTerm(1, 3); // 3x
 
-        p.addTerm(2, 1);  // x^2
-        p.addTerm(0, -1); // -1
+    p.addTerm(2, 1);  // x^2
+    p.addTerm(0, -1); // -1
 
-        Polynomial q(p);  // x^2 + 3x - 1
-        q.addTerm(1, -3); // -3x
+    Polynomial q(p);  // x^2 + 3x - 1
+    q.addTerm(1, -3); // -3x
 
-        cout << "P(x) = " << p << endl;
-        cout << "P(1) = " << p.evaluate(1) << endl;
-        cout << "Q(x) = " << q << endl;
-        cout << "Q(1) = " << q.evaluate(1) << endl;
-        cout << "(P + Q)(x) = " << p + q << endl;
-        cout << "(P * Q)(x) = " << p * q << endl;
-    }
+    cout << "P(x) = " << p << endl;
+    cout << "P(1) = " << p.evaluate(1) << endl;
+    cout << "Q(x) = " << q << endl;
+    cout << "Q(1) = " << q.evaluate(1) << endl;
+    cout << "(P + Q)(x) = " << p + q << endl;
+    cout << "(P * Q)(x) = " << p * q << endl;
+}
