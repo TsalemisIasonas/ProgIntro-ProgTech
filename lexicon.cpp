f9091ffbe1cc2e10a8033d2e8fc85b2090bd4552
TsalemisIasonas <tsalemisi@gmail.com>
2023-06-07 20:20:05 +0300

    test

--- lexicon.cpp
+++ lexicon.cpp
@@ -172,3 +172,19 @@ private:
         }
     }
 };
+
+int main() {
+ lexicon l;
+ l.insert("the");
+ l.insert("boy");
+ l.insert("and");
+ l.insert("the");
+ l.insert("wolf");
+ cout << "The word ’the’ is found " << l.lookup("the") << " time(s)" << endl;
+ cout << "The word ’and’ is found at depth " << l.depth("and") << endl;
+ cout << l;
+ l.replace("boy", "wolf");
+ cout << "After replacement:\n";
+ cout << l;
+ cout << "Now the word ’and’ is found at depth " << l.depth("and") << endl;
+ }
